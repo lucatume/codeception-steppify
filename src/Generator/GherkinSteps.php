d6398a9a81088d159aac64faf727ee8b0faaa991
Luca Tumedei <luca@theaveragedev.com>
2016-12-08 12:54:53 +0100

    remove utility method from template

--- src/Generator/GherkinSteps.php
+++ src/Generator/GherkinSteps.php
@@ -21,33 +21,6 @@ namespace {{namespace}}_generated;
 
 trait {{name}}GherkinSteps{{postfix}}
 {
-        
-    /**
-     * [!] Utility method is generated from steppify task.
-     *
-     * Converts any TableNode found in an array to an array of associative arrays.
-     *
-     * @param array \$args An array of arguments that should be parsed to convert TableNode
-     *              to arrays.
-     * @param array \$iterations Passed by reference; will be set to empty array if there
-     *              there are no TableNode arguments among the arguments, will be set to
-     *              an array of function call arguments if found.
-     */
-    public function _convertTableNodesToArrays(array \$args, &\$iterations = []) {
-        foreach(\$args as \$key => \$value) {
-            if(is_a(\$value, 'Behat\Gherkin\Node\TableNode')){
-                \$rows = \$value->getRows();
-                \$keys = array_shift(\$rows);
-                \$array_value = array_map(function(array \$row) use (\$keys) {
-                    return array_combine(\$keys,\$row);
-                }, \$rows);
-                
-                \$iterations[] = array_replace(\$args, [\$key => \$array_value]);
-            }
-        }
-        
-        return \$args;
-    }
     
     {{methods}}
 }
